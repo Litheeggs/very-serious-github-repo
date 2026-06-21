d09862e87a257972eb1926f4da97a66f70532f27
DefinitelyMaybe <DefinitelyMaybe@users.noreply.github.com>
2026-06-22 11:45:41 +1200

    test

--- sotera/assets/scenes/stage_exit_area.gd
+++ sotera/assets/scenes/stage_exit_area.gd
@@ -8,7 +8,7 @@ func _ready() -> void:
 	body_exited.connect(_on_body_exited)
 	
 func _on_body_entered(body: Node2D):
-	print("entered")
+	print("entered area")
 
 func _on_body_exited(body: Node2D):
-	print("exited")
+	print("exited area")
