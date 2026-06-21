08f2b533f33285507f2ce1e4e34c7b4d8b5bdecf
GreyTheFoxx <ararcupcake@gmail.com>
2026-06-21 16:21:03 -0700

    Added to Main Menu
    Added textured start button.
    Added volume slider.
    Added background placeholder.

---
+++ sotera/assets/scenes/main_menu.gd
@@ -0,0 +1,20 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_play_pressed() -> void:
+	pass # enter game
+
+
+func _on_volume_slider_value_changed(value: float) -> void:
+	# set master volume using scroll
+	AudioServer.set_bus_volume_db(0, linear_to_db(value))
