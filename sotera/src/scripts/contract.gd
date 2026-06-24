7ec40ae974afd898170e824896278eb2fdb2b39c
litheegg <lijunsgbest@gmail.com>
2026-06-24 18:06:50 +0800

    contract now needs to be pickup
    contract now needs to be pickup

--- sotera/src/scripts/contract.gd
+++ sotera/src/scripts/contract.gd
@@ -4,11 +4,25 @@ extends Node2D
 # TODO make a UI counter so that they can see how many contracts they have
 
 var CanPickUp = false
+var button_pressed = false
+
+func _ready():
+	$Button.visible = false
+	Events.contract_standardMaze=true
+	Events.change_level("res://assets/scenes/FortuneWheelScene.tscn")
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	CanPickUp = true
-	# this is placeholder until we sort more out
-	Events.change_level("res://assets/scenes/FortuneWheelScene.tscn")
+	if CanPickUp==true:
+		$Button.visible = true
 
 func _on_area_2d_body_exited(body: Node2D) -> void:
 	CanPickUp = false
+	$Button.visible = false
+
+
+func _on_button_pressed() -> void:
+	Events.contract_standardMaze=true
+	Events.total_contract += 1
+	var button_pressed = true
+	Events.change_level("res://assets/scenes/FortuneWheelScene.tscn")
